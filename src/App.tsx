b24de04c3b0fdf75e63bc8378fd665d515f13be7
Daniel Villaverde <j.danielv@outlook.com>
2024-11-24 22:57:18 -0800

    add initial stage and markup for one building

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <div className='main-wrapper'>
-        <div className="main-wrapper__upper"></div>
+        <div className="main-wrapper__upper">
+          <div className="building">
+            <div className="building__front">
+              <div className="building__front-floor">
+                <div className="building__front-window"></div>
+              </div>
+            </div>
+            <div className="building__side"></div>
+          </div>
+        </div>
         <div className="main-wrapper__lower"></div>
       </div>
     </>
